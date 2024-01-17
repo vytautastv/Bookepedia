f5b6bfc6c9fa69129489204232e616f19d55e38a
QS <qaleh.saleh@gmail.com>
2024-01-17 14:36:58 +0000

    Added changes to the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import '@emotion/styled';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+

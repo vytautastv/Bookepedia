0d300bcc21b5715ee74fe5ce9c54809273832d2b
IH <ikran.h@gmail.com>
2024-01-18 20:57:33 +0000

    added footer import

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import BookSearch from './components/BookSearch';
 import BookCards from './components/BookCards';
+import Footer from './components/Footer';
 
 
 
